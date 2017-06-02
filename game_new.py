a919a5d556a02d939ebacb68568ada6f13e85f37
Ichebykin <chebykin1994@gmail.com>
2017-06-02 17:18:36 +0500

    interact

--- game_new.py
+++ game_new.py
@@ -1,6 +1,9 @@
 import sys
 import pygame
 import numpy as np
+import numpy.linalg as la
+import copy
+import math
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -8,11 +11,12 @@ BLUE = (0, 0, 255)
 pygame.init()
 surface = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
-g = np.array([0,100.0])
+g = np.array([0, 100.0])
 
 
 class Ball:
     def __init__(self, color, radius, x, y, vx, vy):
+        self.m = 1.
         self.color = color
         self.radius = radius
         self.r = np.array([x, y])
@@ -34,22 +38,26 @@ class Ball:
         surface.blit(self.surface, self.r)
 
     def change_v(self, xc, yc):
-        self.v[0] += xc - self.r[0]
-        self.v[1] += yc - self.r[1]
+        self.v += [xc, yc] - self.r
 
-    def interact(self, other):
+    def interact(self, other,surface):
         if self is other:
             return
-        if ((self.r[0] - other.r[0]) ** 2 + (self.r[1] - other.r[1]) ** 2) ** 0.5 <= self.radius + other.radius:
-            # self.
+        if la.norm(self.r - other.r) <= self.radius + other.radius:
+            v = self.v - other.v
+            r = other.r - self.r
+            tau = np.array([-r[1], r[0]]) / la.norm(r)
+            alpha = math.acos(np.dot(v, r) / la.norm(v) / la.norm(r))
+            v_r = la.norm(v) * math.cos(alpha) * r / la.norm(r)
+            v_tau = la.norm(v) * math.sin(alpha) * tau / la.norm(tau)
+            v = - v_r + v_tau
+            self.v = v + other.v
+            other.v = v_r + other.v
 
-            self.v = - self.v
-
-
-ball = Ball(RED, 20, 400., 100., 0., 0.)
-ball2 = Ball(GREEN, 30, 500., 130., 0., 0.)
-ball3 = Ball(BLUE, 20, 100., 123., 0., 0.)
-balls = [ball, ball2, ball3]
+ball1 = Ball(RED, 10, 400., 100., 0., 0.)
+ball2 = Ball(GREEN, 10, 500., 130., 0., 0.)
+# ball3 = Ball(BLUE, 20, 100., 123., 0., 0.)
+balls = [ball1, ball2]
 dt = 0.02
 while True:
     for event in pygame.event.get():
@@ -61,10 +69,12 @@ while True:
             for ball in balls:
                 ball.change_v(xc, yc)
     surface.fill((0, 0, 0))
+    balls2 = copy.copy(balls)
     for ball in balls:
         ball.move(dt)
         ball.draw(surface)
-        for other_ball in balls:
-            ball.interact(other_ball)
+        for other_ball in balls2:
+            ball.interact(other_ball,surface)
+        balls2.remove(ball)
     clock.tick(50)
     pygame.display.update()
---
+++ test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+v = np.array([3, 4])
+r = np.array([1,2])
+print(np.dot(v,r))
