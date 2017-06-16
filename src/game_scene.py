d8825fcda23bcfa6e164ec89c6af15b3bed78db8
Ichebykin <chebykin1994@gmail.com>
2017-06-16 18:00:47 +0500

    small update

--- src/game_scene.py
+++ src/game_scene.py
@@ -19,21 +19,22 @@ left2 = False
 kick2 = False
 move2 = [up2,right2,left2,kick2]
 
-Match = 60000 * 1.5  # 1 minute in milliseconds
+Match = 60000 * 0.1  # 1 minute in milliseconds
 countdown_timer = Parts.Countdown_timer(Match)
 Startpoint_Ball = [400., 200.]
 Startpoint_player_team1 = [780., 280.]
 Startpoint_player_team2 = [20., 280.]
 
-ball = Parts.Ball(Startpoint_Ball, [ 0., 0.])
-player1 = Parts.Player(Startpoint_player_team1, 1)
-player2 = Parts.Player(Startpoint_player_team2, 2)
-scoreboard = Parts.Scoreboard([0, 0])
+
 
 cloud_effect1 = Effects.Cloud_effect([400., 200.])
 cloud_effect2 = Effects.Cloud_effect([400., 200.])
 cloud_effect3 = Effects.Cloud_effect([400., 200.])
 class GameScene(lib.Scene):
+	ball = Parts.Ball(Startpoint_Ball, [ 0., 0.])
+	player1 = Parts.Player(Startpoint_player_team1, 1)
+	player2 = Parts.Player(Startpoint_player_team2, 2)
+	scoreboard = Parts.Scoreboard([0, 0])
 	def _event(self, event):
 		global move1
 		global move2
@@ -76,22 +77,40 @@ class GameScene(lib.Scene):
 				move1[3] = True
 
 	def _update(self,dt):
+		if self.ball.r[0] <= 45 and self.ball.r[1] >= 190:
+			self.scoreboard.goal_team2()
+			self.ball = Parts.Ball(Startpoint_Ball, [ 0., 0.])
+			self.player1.r = Startpoint_player_team1
+			self.player2.r = Startpoint_player_team2
+			pygame.time.delay(1000)
+		if self.ball.r[0] >= 744 and self.ball.r[1] >= 190:
+			self.scoreboard.goal_team1()
+			self.ball = Parts.Ball(Startpoint_Ball, [ 0., 0.])
+			self.player1.r = Startpoint_player_team1
+			self.player2.r = Startpoint_player_team2
+			pygame.time.delay(1000)
 		
 		self.display.fill((255, 255, 255))
 		self.display.blit(bg_image, (0, 0))
-		ball.update(self.display)
-		player1.update(move1, self.display)
-		player2.update(move2, self.display)
-		scoreboard.update(self.display)
+		self.ball.update(self.display)
+		self.player1.update(move1, self.display)
+		self.player2.update(move2, self.display)
+		self.scoreboard.update(self.display)
 		countdown_timer.update(self.display)
-		if pygame.sprite.collide_rect(ball, player1):
-			Physics.interact(ball, player1)
-		if pygame.sprite.collide_rect(ball, player2):
-			Physics.interact(ball, player2)
+		cloud_effect3.r = copy.copy(cloud_effect2.r)
+		cloud_effect2.r = copy.copy(cloud_effect1.r)
+		cloud_effect1.r = copy.copy(self.ball.r)
+		if pygame.sprite.collide_rect(self.ball, self.player1):
+			Physics.interact(self.ball, self.player1)
+		if pygame.sprite.collide_rect(self.ball, self.player2):
+			Physics.interact(self.ball, self.player2)
 		if move1[3]:
-			player1.kick(ball)
+			self.player1.kick(self.ball)
 		if move2[3]:
-			player2.kick(ball)	
+			self.player2.kick(self.ball)	
 		move1[3] = False
-		move2[3] = False	
+		move2[3] = False
+		if pygame.time.get_ticks()>=Match:
+			self.__end = True
+			self.set_next_scene(None)
 	
--- src/lib/ResManager.py
+++ src/lib/ResManager.py
@@ -2,12 +2,12 @@ import os
 import pygame
 
 class ResManager: #класс загрузки ресурсов
-	def __init__(self, data_dir = 'data',image_dir = '../data/image',sound_dir = 'sound',music_dir = 'music'):
+	def __init__(self, data_dir = 'data',image_dir = '../data/image',sound_dir = 'sound',music_dir = '../data/music'):
 		self.data_dir = data_dir #каталог ресурсов
 		self.image_dir = image_dir #каталог изображений
 		self.sound_dir = sound_dir #каталог звуков
 		self.music_dir = music_dir #каталог музыки
-           #...........
+
 	def get_image(self, name): #метод загрузки файла по имени
 		#fullname = os.path.join(self.data_dir, os.path.join(self.image_dir, name)) #получение имени нужного файла вместе с путями к нему
 		fullname = os.path.join(os.path.join(self.image_dir, name))
